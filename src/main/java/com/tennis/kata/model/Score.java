cb09b3f3391a848d4aa77d2b58437f1a30e203d2
Amin Jellali <a.j.aminjellali@gmail.com>
2026-05-05 20:08:43 +0200

    Add to string to respect the specs

--- src/main/java/com/tennis/kata/model/Score.java
+++ src/main/java/com/tennis/kata/model/Score.java
@@ -1,7 +1,7 @@
 package com.tennis.kata.model;
 
 public enum Score {
-    LOVE(0), FIFTEEN(15), THIRTY(30), FORTY(40), ADVANTAGE(40);
+    LOVE(0), FIFTEEN(15), THIRTY(30), FORTY(40), ADVANTAGE(50), WIN(100);
 
     private final int value;
 
@@ -15,10 +15,15 @@ public enum Score {
 
     public Score next() {
         Score[] values = values();
-        int nextOrdinal = this.ordinal() + 1;
+        int nextOrdinal = ordinal() + 1;
         if (nextOrdinal >= values.length) {
-            throw new IllegalStateException("Cannot advance beyond ADVANTAGE");
+            throw new IllegalStateException("Cannot advance beyond WIN.");
         }
         return values[nextOrdinal];
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
     }
 }
